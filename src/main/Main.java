751366e68fc79222029955f9480812dbf292f66b
qiantao <qiantao@aikalife.com>
2017-10-11 11:44:29 +0800

    Revert "改用Kotlin编写"
    This reverts commit 9bc4fbb

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import category.Array;
+
+import java.util.Arrays;
+
+/**
+ * Created by qiantao on 2017/2/3.
+ * 示例程序
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        int[][] arr = Array.pascalTriangle(5);
+        System.out.println(Arrays.deepToString(arr));
+        System.out.println(Arrays.generate(5).toString());
+    }
+}
