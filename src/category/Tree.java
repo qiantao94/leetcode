e2b074da213dd07cd8121daf2294644eaa7aabcf
bestTao <1105338951@qq.com>
2017-10-12 17:15:29 +0800

    617. Merge Two Binary Trees

---
+++ src/category/Tree.java
@@ -0,0 +1,27 @@
+package category;
+
+public class Tree {
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 合并二叉树，同一节点值相加
+     * 617. Merge Two Binary Trees
+     */
+    public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
