228498d07896408850c04f7be2045bf0d7dab75f
qiantao <tao_qian@dingyuegroup.cn>
2017-11-13 16:40:15 +0800

    538. Convert BST to Greater Tree

--- src/category/Tree.java
+++ src/category/Tree.java
@@ -54,4 +54,28 @@ public class Tree {
 
         return root;
     }
+
+    /**
+     * 二叉树转换
+     * 538. Convert BST to Greater Tree
+     */
+    public static TreeNode convertBST(TreeNode root){
+        int sum = 0;
+        TreeNode node = root;
+        Stack<TreeNode> stack = new Stack<>();
+
+        while(!stack.isEmpty() || node != null){
+            while(node != null){
+                stack.add(node);
+                node = node.right;
+            }
+            node = stack.pop();
+            sum += node.val;
+            node.val = sum;
+
+            node = node.left;
+        }
+
+        return root;
+    }
 }
